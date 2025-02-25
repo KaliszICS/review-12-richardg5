b5dfc416e16582b12d932b4089377fbc91aa6b48
richardg5 <richard.gao5@student.tdsb.on.ca>
2025-02-25 18:23:29 +0000

    Add code

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,6 +1,36 @@
 public class PracticeProblem {
 
 	public static void main(String args[]) {
+		System.out.println(isPalindrome("aaabaaa"));
+	}
 
+	public static boolean isPalindrome(String word) {
+		word = word.toLowerCase().replaceAll(" ", "");
+		for (int i = 0; i < word.length() / 2; i++) {
+			if (word.charAt(i) != word.charAt(word.length() - 1 - i)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public static int getAge(String[] names, int[] ages, String name) {
+		for (int i = 0; i < names.length; i++) {
+			if (names[i] == name) {
+				return ages[i];
+			}
+		}
+		return -1;
+	}
+
+	public static int countWords(String words, char search) {
+		String[] wordArr = words.split(" ");
+		int count = 0;
+		for (String currWord : wordArr) {
+			if (currWord.indexOf(search) != -1) {
+				count++;
+			}
+		}
+		return count;
 	}
 }
